1168fab16d4b04eed447aacc0e59f40bc0b6c8c2
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-21 22:04:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 def prime?(num)
-  num.each do |i| 
-end
-
-
-(2..(num - 1)).each do |n|
-    return false if num % n == 0
-  end
+  (2..(num - 1)).each do |i|
+    return false if num % i == 0 
+  end 
   true
 end
+
+
